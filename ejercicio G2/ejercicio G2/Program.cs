46a05e80816e178d1fe8aadc9e01bf0ba07ee524
kevin alexander <no0336032020@unab.edu.sv>
2021-05-09 12:16:25 -0600

    ejercicio grupo 2

---
+++ ejercicio G2/ejercicio G2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_G2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            byte edad;
+            string sexo;
+           
+            Console.WriteLine("ingrese nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese la edad");
+            edad = byte.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el sexo Masculino=m y femenino=f");
+            sexo = Console.ReadLine();
+
+
+            if (sexo == "m")
+            {
+
+                Console.WriteLine("el Sr." + nombre + " se ha registrado");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("La Sra." + nombre + " se ha sido registrada");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\n----->Fin del programa");
+            Console.ReadKey();
+        }
+    }
+}
